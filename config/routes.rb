767aba12ed2fc036815731fc45229d3c5c705294
Stanislav <wildkain@gmail.com>
2018-09-10 02:29:05 +0300

    Add some style

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'home#index', as: :home
+  root to: 'home#index', as: :root
   resources :currencies, only: %i(new create update)
   get 'admin', to: 'currencies#new'
   mount ActionCable.server => '/cable'
